737cf049281319f9b11bdc4c3bd76d5fcffb5c71
Alberto Rosas <alberto.rsesc@protonmail.com>
2024-08-26 08:42:13 -0700

    Google Analytics added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="!scroll-smooth">
     <head>
+        @production
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-H1NBRRQX1S"></script>
+            <script>
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'G-H1NBRRQX1S');
+            </script>
+        @endproduction
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
